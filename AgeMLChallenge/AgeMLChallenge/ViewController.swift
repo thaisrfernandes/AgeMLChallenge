9dbe129f8bfcd135da9702a367ba62aff5b1055a
Thaís Fernandes <throdriguesfernandes@gmail.com>
2022-05-04 16:03:56 -0300

    model working

--- AgeMLChallenge/AgeMLChallenge/ViewController.swift
+++ AgeMLChallenge/AgeMLChallenge/ViewController.swift
@@ -6,14 +6,71 @@
 //
 
 import UIKit
+import CoreML
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        getAge()
     }
-
-
+    
+    func getAge() {
+        let image = UIImage(named: "crianca")?.resize(size: CGSize(width: 227, height: 227))
+        
+        do {
+            let config = MLModelConfiguration()
+            
+            let model = try AgeNet(configuration: config)
+            
+            let input = AgeNetInput(data: (image?.toCVPixelBuffer())!)
+            
+            let output = try model.prediction(input: input)
+            
+            print(String(describing: output.classLabel))
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+    }
 }
 
+extension UIImage {
+    func resize(size: CGSize) -> UIImage? {
+        UIGraphicsBeginImageContext(size)
+        draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
+    
+    func toCVPixelBuffer() -> CVPixelBuffer? {
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer : CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(self.size.width), Int(self.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard status == kCVReturnSuccess else {
+            return nil
+        }
+        
+        if let pixelBuffer = pixelBuffer {
+            CVPixelBufferLockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+            let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer)
+            
+            let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+            let context = CGContext(data: pixelData, width: Int(self.size.width), height: Int(self.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+            
+            context?.translateBy(x: 0, y: self.size.height)
+            context?.scaleBy(x: 1.0, y: -1.0)
+            
+            UIGraphicsPushContext(context!)
+            self.draw(in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
+            UIGraphicsPopContext()
+            CVPixelBufferUnlockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+            
+            return pixelBuffer
+        }
+        
+        return nil
+    }
+}
